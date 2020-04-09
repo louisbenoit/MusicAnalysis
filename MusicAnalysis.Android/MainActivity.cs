3de5f713a56b4d346efc8a5651b37dc7b8a06f7e
kootsoop <peter@kootsoop.com>
2020-04-09 11:51:10 -0400

    Added App icon different from the default.

--- MusicAnalysis.Android/MainActivity.cs
+++ MusicAnalysis.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MusicAnalysis.Droid
 {
-    [Activity(Label = "MusicAnalysis", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MusicAnalysis", Icon = "@drawable/music_analysis", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
