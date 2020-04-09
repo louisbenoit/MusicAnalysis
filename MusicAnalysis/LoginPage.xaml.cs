12ab4780dcc6cccb36a3025947606764b276d41a
Louis Benoit <louis.benoit@student.fairfield.edu>
2020-04-09 16:38:31 -0400

    Added App Icon Pictures for iOS

--- MusicAnalysis/LoginPage.xaml.cs
+++ MusicAnalysis/LoginPage.xaml.cs
@@ -13,14 +13,15 @@ namespace MusicAnalysis
         }
         async void UsernameCompleted(object sender, EventArgs e)
         {
-            string text = ((Entry)sender).Text;
+            string username = ((Entry)sender).Text;
         }
         async void PasswordCompleted(object sender, EventArgs e)
         {
-            string text = ((Entry)sender).Text;
+            string password = ((Entry)sender).Text;
         }
         private async void LoginClicked(object sender, EventArgs e)
         {
+            await Navigation.PushAsync(new MainPage());
             //GET https://accounts.spotify.com/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09
             //await Navigation.PushAsync(new MainPage());
         }
